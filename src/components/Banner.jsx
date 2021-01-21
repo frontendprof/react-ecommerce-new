016b50853a24257da40a2d605f88516c52684b7e
frontendprof <https://github.com/frontendprof>
2021-01-21 11:40:45 +0500

    Styling banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,13 @@ const Banner = () => {
     return (
         <div className="container">
             <header>
-                
+                <div className="headerText">
+                    <div>
+                        <h1>Iyd ayyomingiz muborak bo'lsin</h1>
+                        <p>Biz bilan savdo qiling wa qulayliklardan rohatlaning!</p>
+
+                    </div>
+                </div>
             </header>
         </div>
     )
